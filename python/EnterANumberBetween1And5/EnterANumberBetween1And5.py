e4c586b8e22708fbdb9d84835ac6c4227812a7a1
EricJPogue <epogue@epogue.com>
2021-10-22 11:50:19 -0500

    Updated EnterAnNumberBetween1And5.

--- python/EnterANumberBetween1And5/EnterANumberBetween1And5.py
+++ python/EnterANumberBetween1And5/EnterANumberBetween1And5.py
@@ -16,36 +16,36 @@
 # Licensing
 # Reuse
 
-
 def introduction():
-	print("Introduction...")
+	print("Introduction:... ")
 
-def requestAValidNumberBetween1And5():
-	UserResponse2String = str(input("Enter a number between 1 and 5.\n"))
+def getAValidNumberBetteen1And5():
+	UserResponse2String = str(input(("Enter a number between 1 and 5.\n")))
 	print("You entered: " + UserResponse2String)
 
 	if not UserResponse2String.isnumeric():
-		print("This is not a number!")
-		return False
+		print("This is not a number.")
+		return -1
 	else:
-		# print("This a valid number")
+		print("This is a valid number.")
 		UserResponse2Int = int(UserResponse2String)
-		if (UserResponse2Int < 1) or (UserResponse2Int > 5):
-			print("\nThis is not a number between 1 and 5.")
-			return False
+		if (UserResponse2Int >= 1) and (UserResponse2Int <= 5):
+			print("You have entered a number between 1 and 5. Great job!")
+			return UserResponse2Int
 		else:
-			print("\nThis is a number between 1 and 5. Great job!")
-			return True
+			print("Your number is not between 1 and 5.")
+			return -1
 
-
-# Program starts here. 
+# Program starts executing here. 
 introduction()
 
-userHasEnteredANumberBetween1And5 = False
-while not userHasEnteredANumberBetween1And5:
-	userHasEnteredANumberBetween1And5 = requestAValidNumberBetween1And5()
-	print(userHasEnteredANumberBetween1And5)
-	if not userHasEnteredANumberBetween1And5:
-		print("Please try again.")
+number = -1 # The user has not yet enter a valid number between 1 and 5.
+while (number == -1):
+	number = getAValidNumberBetteen1And5()
+	if (number == -1):
+		print("Please try again.\n")
+
+print("\nValid number enter. The number was: ")
+print(number)
 
 
